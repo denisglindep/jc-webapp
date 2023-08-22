e91c098f47661b6f55492f4357b8075eb788ae80
Denys <f.denys@improveit.solutions>
2023-08-22 15:07:37 +0300

    Forgot password functionality

--- src/components/screens/ForgotPassword.vue/ForgotPasswordForm.vue
+++ src/components/screens/ForgotPassword.vue/ForgotPasswordForm.vue
@@ -10,94 +10,62 @@
       closable
       :text="errors.apiError"
     />
-    <v-form @submit="resetPassword">
-      <div class="text-center" v-if="!showOtp">
-        <v-img>
-          <v-icon icon="mdi-key-outline" size="x-large" />
-        </v-img>
-        <v-card-title>Forgot password?</v-card-title>
-        <v-card-text>
-          <v-card-subtitle>No worries, we'll send you reset instructions.</v-card-subtitle>
-        </v-card-text>
+    <v-form @submit="resetPassword" class="text-center">
+      <v-img>
+        <v-icon icon="mdi-key-outline" size="x-large" />
+      </v-img>
+      <v-card-title>Forgot password?</v-card-title>
+      <v-card-text>
+        <v-card-subtitle>No worries, we'll send you reset instructions.</v-card-subtitle>
+      </v-card-text>
 
-        <v-card-item class="pa-0">
-          <v-text-field
-            v-bind="email"
-            class="mb-2 text-start"
-            clearable
-            density="compact"
-            label="Email"
-            prepend-inner-icon="mdi-email-outline"
-          />
-        </v-card-item>
-        <v-card-actions>
-          <v-btn
-            block
-            variant="flat"
-            size="x-large"
-            @click="handleEmail"
-            :disabled="!!errors?.email"
-          >
-            Send verification code
-          </v-btn>
-        </v-card-actions>
+      <v-card-item class="pa-0">
+        <v-text-field
+          v-bind="email"
+          class="mb-2 text-start"
+          clearable
+          density="compact"
+          label="Email"
+          prepend-inner-icon="mdi-email-outline"
+        />
+      </v-card-item>
+      <v-card-actions>
+        <v-btn
+          block
+          variant="flat"
+          size="x-large"
+          type="submit"
+          :loading="isSubmitting"
+          :disabled="!meta?.valid || isSubmitting"
+        >
+          Reset password
+        </v-btn>
+      </v-card-actions>
 
-        <v-card-text class="pa-0" :class="[isDarkMode ? 'text-grey' : 'text-primary']">
-          <v-btn
-            to="/signin"
-            size="x-small"
-            variant="plain"
-            color="inherit"
-            prepend-icon="mdi-chevron-left"
-          >
-            Back to Log In
-          </v-btn>
-        </v-card-text>
-      </div>
-      <div class="text-center" v-else>
-        <v-img>
-          <v-icon icon="mdi-lock-check" size="x-large" />
-        </v-img>
-        <v-card-title>Verification code sent!</v-card-title>
-
-        <v-card-text>
-          <v-card-subtitle>
-            We sent you a one-time password (OTP) to your phone and to your email address.
-          </v-card-subtitle>
-          <v-card-subtitle>
-            Please check your SMS or your Email inbox and input the OTP below.
-          </v-card-subtitle>
-        </v-card-text>
-        <v-card-item class="pa-0">
-          <v-text-field
-            v-bind="otp"
-            class="text-start mb-2"
-            clearable
-            density="compact"
-            label="Verification code"
-            prepend-inner-icon="mdi-key-outline"
-          />
-        </v-card-item>
-        <v-card-actions>
-          <v-btn block variant="flat" size="x-large" type="submit" :disabled="!!errors?.otp">
-            Reset password
-          </v-btn>
-        </v-card-actions>
-      </div>
+      <v-card-text class="pa-0" :class="[isDarkMode ? 'text-grey' : 'text-primary']">
+        <v-btn
+          to="/signin"
+          size="x-small"
+          variant="plain"
+          color="inherit"
+          prepend-icon="mdi-chevron-left"
+        >
+          Back to Log In
+        </v-btn>
+      </v-card-text>
     </v-form>
-    <v-dialog v-model="showEmailSentDialog" width="auto" max-width="50%">
+    <v-dialog v-model="showEmailSentDialog" width="auto" max-width="50%" class="text-center">
       <v-card class="px-4 py-2">
-        <v-card-title class="mb-4"
-          >Link sent successfully! <v-icon icon="mdi-success-outlined"
-        /></v-card-title>
-        <v-card-subtitle>We had sent reset password link to your registered email.</v-card-subtitle>
+        <v-card-title>Link sent successfully! <v-icon icon="mdi-success-outlined" /></v-card-title>
+        <v-card-subtitle>We had sent reset password link to your email.</v-card-subtitle>
         <v-card-actions class="d-flex">
           <v-btn
+            block
             class="w-50"
             rounded="lg"
             color="primary"
             variant="flat"
-            @click="showEmailSentDialog = false"
+            @click="handleEmailSentDialog"
           >
             OK
           </v-btn>
@@ -111,20 +79,21 @@ import { computed, ref } from 'vue';
 import { useForm } from 'vee-validate';
 import { object, string } from 'yup';
 import { useTheme } from 'vuetify';
+import { useRouter } from 'vue-router';
 import { useAuth } from '@/stores/auth';
 
-const showOtp = ref(false);
+const showEmailSentDialog = ref(false);
 
+const router = useRouter();
 const authStore = useAuth();
 const theme = useTheme();
 const isDarkMode = computed(() => theme.current.value.dark);
 
 const schema = object({
-  email: string().email().required(),
-  otp: string().required().min(4).max(4)
+  email: string().email().required().label('E-mail')
 });
 
-const { handleSubmit, defineComponentBinds, errors, setErrors, values } = useForm({
+const { handleSubmit, defineComponentBinds, errors, setErrors, meta, isSubmitting } = useForm({
   validationSchema: schema
 });
 
@@ -135,9 +104,8 @@ const vuetifyConfig = (state) => ({
 });
 
 const email = defineComponentBinds('email', vuetifyConfig);
-const otp = defineComponentBinds('otp', vuetifyConfig);
 
-async function handleEmail() {
+const resetPassword = handleSubmit(async (values) => {
   try {
     const formData = new FormData();
     formData.append('phone_number', values?.email);
@@ -146,14 +114,15 @@ async function handleEmail() {
       phone_number: values?.email,
       type: 'EMAIL'
     });
-    showOtp.value = true;
+    showEmailSentDialog.value = true;
   } catch (error) {
     setErrors({ apiError: error?.message });
     setTimeout(() => setErrors({ apiError: null }), 5000);
   }
-}
-
-const resetPassword = handleSubmit(async (values) => {
-  console.log(123);
 });
+
+const handleEmailSentDialog = () => {
+  showEmailSentDialog.value = false;
+  router.push('/signin');
+};
 </script>
